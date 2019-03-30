b5962e37cca68ebf80122e1e10f397628b775caa
Noklish <nschweser@smu.edu>
2019-03-29 19:12:22 -0500

    pushing to dev

--- pantryapp/src/App.js
+++ pantryapp/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            I would like to die
           </a>
         </header>
       </div>
