bb08f438f714d2ed05cd09999d6ac4b7387d17ef
Harry Holcomb <31421567+holcombharry@users.noreply.github.com>
2019-04-24 19:55:54 -0500

    Delete recipe.js

--- pantryapp/src/models/recipe.js
+++
@@ -1,8 +0,0 @@
-export class Recipe {
-    constructor(name, meal, description, ingredients){
-        this.name = name;
-        this.meal = meal;   //breakfast, lunch, dinner
-        this.description = description;
-        this.ingredients = ingredients;
-    }
-}
