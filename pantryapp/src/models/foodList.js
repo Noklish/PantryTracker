3d6e257d18d32d0890101f6922875879676087f4
Harry Holcomb <31421567+holcombharry@users.noreply.github.com>
2019-04-24 19:55:35 -0500

    Delete foodList.js

--- pantryapp/src/models/foodList.js
+++
@@ -1,9 +0,0 @@
-export class FoodList {
-    constructor(food, brand, type, expire, quantity){
-        this.food = food;
-        this.brand = brand;
-        this.type = type;
-        this.expire = expire;
-        this.quantity = quantity;
-    }
-}
