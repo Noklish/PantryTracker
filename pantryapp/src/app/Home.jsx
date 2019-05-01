a4087f40646f178a9c113f11c58f40035b6c492e
Harry Holcomb <31421567+holcombharry@users.noreply.github.com>
2019-05-01 16:05:32 -0500

    Update Home.jsx

--- pantryapp/src/app/Home.jsx
+++ pantryapp/src/app/Home.jsx
@@ -4,7 +4,7 @@ import './Home.css';
 
 
 export const Home = (props) => (
-    <h1 id="home-alert" className="alert alert-info text-white text-center">{`Welcome to Pantry Tracker, ${props.user.username}!`}</h1>
+    <h1 id="home-alert" className="alert alert-info text-center">{`Welcome to Pantry Tracker, ${props.user.username}!`}</h1>
 )
 
 export default Home;
