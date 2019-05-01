7cd39b2338fcd9319d7948ba66e4ca08a4e41bf1
Noklish <nschweser@smu.edu>
2019-05-01 02:43:45 -0500

    allow pull

---
+++ pantryapp/src/app/Ingredients.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { RecipeModal } from './modals/RecipeModal';
+import { repository } from './../api/repository';
+
+class Ingredients extends React.Component {
+    repo = new repository();
+
+    state = {
+      tableList: []
+    }
+
+    render() {
+        return (
+            <>
+            {
+                this.state.tableList.map((n, j) => 
+                    <tr key={j}>
+                        <td>{n.foodName}</td>
+                        <td>{n.quantity}</td>
+                    </tr>
+                )
+            }
+            </>
+        )
+    }
+
+    componentDidMount(){
+        this.repo.getIngredients(this.props.recipeId).then(ingredients => {
+              this.setState(state => ({
+                  tableList: ingredients}));
+        })
+    } 
+    
+}
+
+export default Ingredients;
